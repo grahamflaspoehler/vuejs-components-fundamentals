98132901e974fa4fb92cdbd90621ee55d1a15ec1
unknown <graham.flaspoehler@paradyme.us>
2020-03-31 17:14:48 -0400

    Lots of changes

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,30 +1,57 @@
-let PlanComponent = {
-  template: "#plan-template",
+// ===== Plan ===== 
+let PlanPickerItemComponent = {
+  template: "#plan-picker-item-template",
 
   props: {
     name: {
       type: String,
       required: true
+    }, 
+    
+    selectedPlan: {
+      type: String
+    }
+  }, 
+
+  computed: {
+    isSelected(){
+      return this.name === this.selectedPlan;
+    }, 
+
+  }, 
+
+  methods: {
+    select(){
+      this.$emit('select', this.name);
     }
   }
 }; 
 
+
+// ===== Plan-Picker =====
 let PlanPickerComponent = {
   template: "#plan-picker-template", 
 
   components: {
-    plan: PlanComponent,
+    'plan-picker': PlanPickerItemComponent,
   }, 
 
   data() {
     return {
-      plans: ["The Hacker", "The Single", "The Curious", "The Addict"]
+      plans: ["The Hacker", "The Single", "The Curious", "The Addict"], 
+      selectedPlan: null,
+    }
+  }, 
+
+  methods: {
+    selectPlan(plan){
+     this.selectedPlan = plan;
     }
   }
 };
 
 
-
+// ===== Root =====
 new Vue({
   el: "#app",
   components: {
